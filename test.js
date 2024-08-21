ba08705c5cec57a501a00f646512be3eaf4b5bbe
F飞行 <110045310@qq.com>
2024-08-21 10:28:18 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 3000); // 每隔3秒切换到下一张图片
+
 document.getElementById("sendFeedback").addEventListener("click", function() {
     alert("感谢反馈，推荐加Q群963364247");
 });
+
+window.onload = () => {
+    showSlide(currentIndex);
+};
